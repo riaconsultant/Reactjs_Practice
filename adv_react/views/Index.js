61fa08cf5484e866f8d3aca504ade53910c26d46
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2017-11-29 09:41:07 +0530

    class component

---
+++ adv_react/views/Index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var React = require('react');
+var PropTypes = require('prop-types');
+
+var  App= React.createClass({
+
+  render: function() {
+    return (
+      <div />
+    );
+  }
+
+});
+
+module.exports = App;
+
+ReactDOM.render(< App/>, document.body, callback);
