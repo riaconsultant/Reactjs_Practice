14aab9594b723dea8e2ede4f0ed3ff85ea2e8b09
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2021-12-29 09:52:31 +0530

    package added

---
+++ w3cschool/src/components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className="">
+      <p>Hello React App</p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
