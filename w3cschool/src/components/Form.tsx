cfe0e5ccda70b484b5f869316adfca1e929e44d4
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2022-01-09 13:52:40 +0530

    Form+multi form added

--- w3cschool/src/components/Form.tsx
+++ w3cschool/src/components/Form.tsx
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 const Form: React.FC<any> = (props: any) => {
+  const [name, setName] = useState("Manoj");
+
+  const changeMe = (evt: any) => {
+    setName(evt.target.value);
+    console.log(evt.target.value);
+  };
+
+  const formSubmitHandler = (e: any) => {
+    e.preventDefault();
+    console.log("Form Submit", e, name);
+  };
   return (
     <div>
-      <label>React Form</label>
-      <form>
+      <h2>React Form</h2>
+      <form onSubmit={formSubmitHandler}>
         <label>Enter your name:</label>
-        <input type="text" />
+        <input type="text" value={name} onChange={(e) => changeMe(e)} />
+        <input type="submit" />
       </form>
+      <MultiForm />
     </div>
+  );
+};
+
+const MultiForm = () => {
+  const [inputs, setInputs] = useState({ username: "", age: "" });
+
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+    console.log("Multiple Form", e.target, inputs);
+    // setInputs(e.target.value);
+  };
+
+  const changeHandler = (e: any) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(name, value);
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <label>Enter your name</label>
+      <input
+        type="text"
+        name="username"
+        value={inputs.username || ""}
+        onChange={changeHandler}
+      />
+      <br />
+      <label>Age</label>
+      <input
+        type="number"
+        name="age"
+        value={inputs.age || ""}
+        onChange={changeHandler}
+      />
+      <input type="submit" />
+    </form>
   );
 };
 
