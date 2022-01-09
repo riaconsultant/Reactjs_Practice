3be2b4effb168f6785c60646cb6a4db3f82c0655
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2022-01-09 17:04:54 +0530

    css added

--- w3cschool/src/components/Todo.tsx
+++ w3cschool/src/components/Todo.tsx
@@ -1,11 +1,33 @@
 import React, { memo } from "react";
 const Todo = (props: any) => {
   console.log("Child Component", props.todoc);
+  const style = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "5px",
+    cursor: "pointer",
+  };
+
+  const clickHandler = (e: any) => {
+    console.log("Para", e.target.textContent);
+  };
   return (
     <div>
-      <h1>My Todo</h1>
+      <h1
+        style={{
+          color: "red",
+          backgroundColor: "lightblue",
+          border: "1px solid #ccc",
+        }}
+      >
+        My Todo
+      </h1>
       {props.todoc.map((todo: string, index: number) => {
-        return <p key={index}>{todo}</p>;
+        return (
+          <p style={style} key={index} onClick={clickHandler}>
+            {todo}
+          </p>
+        );
       })}
     </div>
   );
--- w3cschool/src/pages/Todos.tsx
+++ w3cschool/src/pages/Todos.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Todo from "../components/Todo";
-
+import styles from "./../styles/style.module.css";
 const Todos = () => {
   const [todos, setTodos] = useState(["Todo 1", "Todo 2"]);
   const [count, setCount] = useState(0);
@@ -17,7 +17,9 @@ const Todos = () => {
       <hr />
       <div>
         Count: {count}
-        <button onClick={increment}>+</button>
+        <button className={styles.bigBtn} onClick={increment}>
+          +
+        </button>
       </div>
     </div>
   );
